fffa01586f7800a58f4870af9d691eb2a71e8be0
geordy409 <geordyngoua2020@gmail.com>
2024-05-19 16:51:39 +0200

    Your commit message here

--- loops.py
+++ loops.py
@@ -1,4 +1,17 @@
+# The loops in Language python
 
 name_list = ["geordy", "diamand", "thales"]
 for name in name_list:
     print(name)
+
+print()   
+
+for i in range(6): # range(5) ou range(0, 5) = {O, 1, 2, 3, 4} range(n) = [o, n - 1] 
+    print(i)
+
+print()
+
+students = ["Hermoine", "Harry", "Ron"]
+
+for i in range(len(students)):
+    print(i , students[i])
